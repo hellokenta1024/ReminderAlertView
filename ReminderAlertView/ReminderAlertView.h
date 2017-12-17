b13b9cc7a2651e7c0f79eec031d96aa5d30b97df
KEN-chan <hellokenta1024@gmail.com>
2017-12-17 21:58:45 +0900

    Add projects

---
+++ ReminderAlertView/ReminderAlertView.h
@@ -0,0 +1,19 @@
+//
+//  ReminderAlertView.h
+//  ReminderAlertView
+//
+//  Created by Kenta Hara on 2017/12/17.
+//  Copyright © 2017 Kenta Hara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReminderAlertView.
+FOUNDATION_EXPORT double ReminderAlertViewVersionNumber;
+
+//! Project version string for ReminderAlertView.
+FOUNDATION_EXPORT const unsigned char ReminderAlertViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReminderAlertView/PublicHeader.h>
+
+
